a493ebdd8c90defcfffb29b45947dd52cae1c96d
hadi77738 <111193242+hadi77738@users.noreply.github.com>
2025-09-15 21:56:14 +0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   host: 'aws-1-ap-southeast-1.pooler.supabase.com',
-  port: 5432,
+  port: 6543,
   database: 'postgres',
   user: 'postgres.ljveqfmifeqquebjvwau',
   password: 'sugihmanik1',
